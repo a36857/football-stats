67041da071031f4158d618f033edf1783575c694
a36857 <36857@alunos.isel.ipl.pt>
2016-02-17 00:44:07 +0000

    v1.1

--- public/js/client/group-create.js
+++ public/js/client/group-create.js
@@ -1,20 +1,30 @@
 "use strict";
 
 $(document).ready(function() {
+   // var submitting = false; //cause some bug when user goes crazy on the submit button
+
     $('#group-form').submit(function(e) {
         e.preventDefault();
+        //if(!submitting) { submitting = true;
 
-        var input = $("#input-group").val();
+            var input = $("#input-group").val();
+            $('#loader').show();
 
-        $.ajax({
-            url     : $(this).attr('action'),
-            type    : $(this).attr('method'),
-            data    : $(this).serialize(),
-            success : function(response) {
-                insertNewGroup(response,input);
-            }
-        });
-
+            $.ajax({
+                url: $(this).attr('action'),
+                type: $(this).attr('method'),
+                data: $(this).serialize(),
+                success: function (response) {
+                    insertNewGroup(response, input);
+                    $('#loader').hide();
+          //          submitting = false;
+                },
+                error: function () {
+                    $('#loader').hide();
+            //        submitting = false;
+                }
+            });
+        //}
         return false;
     });
 
--- public/js/client/group-delete.js
+++ public/js/client/group-delete.js
@@ -6,13 +6,20 @@ $(document).ready(function() {
         e.preventDefault();
 
         var team = $(this).find('[name="team"]').val();
+        $("#t"+team).hide();
+        $("#l"+team).show();
 
         $.ajax({
             url     : $(this).attr('action'),
             type    : $(this).find('[name="_method"]').val(),
             data    : $(this).serialize(),
             success : function(response) {
-                $("#"+team).remove();
+                $("#t"+team).remove();
+                $("#l"+team).hide();
+            },
+            error   : function(){
+                $("#l"+team).hide();
+                $("#t"+team).show();
             }
         });
 
