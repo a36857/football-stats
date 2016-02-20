6da27edf5d380df64ba1adc98e7e55c1c5493eeb
a36857 <36857@alunos.isel.ipl.pt>
2016-02-20 00:21:14 +0000

    v1.2

--- public/js/client/validate-passwords.js
+++ public/js/client/validate-passwords.js
@@ -1,14 +1,38 @@
 "use strict";
 
-function validatePasswords() {
-    var pass = document.forms["registration-form"]["password"].value;
-    var confpass = document.forms["registration-form"]["confirmPassword"].value;
+const passwordLenght = 6;
 
-    if(pass!=confpass) {
+function validatePasswords() {
+    let pass = document.forms["registration-form"]["password"].value;
+    let confpass = document.forms["registration-form"]["confirmPassword"].value;
+
+    if(pass.length < passwordLenght || confpass.length < passwordLenght) {
+        return false;
+    }
+    else if(pass!=confpass) {
         $("#error-password").removeClass('hidden');
         if($('#error').length) $("#error").remove();
         return false;
     }
     else
         return true;
+}
+
+function verifyLength(id,input) {
+    let parent = $('#'+id);
+
+    if(input.length == 0) {
+        parent.find('.ok').addClass('hidden');
+        parent.find('.remove').addClass('hidden');
+    }
+    else {
+        if(input.length < passwordLenght) {
+            parent.find('.ok').addClass('hidden');
+            parent.find('.remove').removeClass('hidden');
+        }
+        else {
+            parent.find('.remove').addClass('hidden');
+            parent.find('.ok').removeClass('hidden');
+        }
+    }
 }
